44e2bf6cd36b99442d6067519ded25245f38b00f
Lala Tseng <z162614@gmail.com>
2024-02-24 14:24:42 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('express app for restaurants')
-})
+app.get("/", (req, res) => {
+  res.redirect("/restaurants");
+});
+
+app.get("/restaurants", (req, res) => {
+  res.send("listing restaurants");
+});
+
+app.get("/restaurant/:id", (req, res) => {
+  const id = req.params.id
+  res.send(`read restaurant: ${id}`)
+});
 
 app.listen(port, () => {
-  console.log(`express server is runnig on http://localhost:${port}`)
-})
+  console.log(`express server is runnig on http://localhost:${port}`);
+});
